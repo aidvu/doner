15681fbcfbad9901472832dd3fb15b941b4ace88
Andrija Vucinic <andrija.vucinic@gmail.com>
2015-06-25 22:18:42 +0200

    Set PDO error mode to Exceptions. Add composer.lock

--- api/Doner/Database/MySql.php
+++ api/Doner/Database/MySql.php
@@ -2,22 +2,26 @@
 
 namespace Doner\Database;
 
-class MySql
-{
+class MySql {
 	// Hold an instance of the class
 	private static $instance;
 
 	// Disable clone function
-	private function __clone() {}
+	private function __clone() {
+	}
 
 	// A private constructor; prevents direct creation of object
-	private function __construct() {}
+	private function __construct() {
+	}
 
 	// The singleton method
 	public static function getInstance() {
-		if (!isset(self::$instance)) {
-			self::$instance = new \PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new \PDO( 'mysql:host=' . HOST . ';dbname=' . DATABASE, USERNAME, PASSWORD );
 		}
+
+		self::$instance->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
+
 		return self::$instance;
 	}
 }

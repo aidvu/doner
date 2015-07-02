08fbee1ed72393c6eb5e6fbea1377dc29db38a94
Andrija Vucinic <andrija.vucinic@gmail.com>
2015-07-02 15:39:36 +0200

    Add /me endpoint for returning the currently logged user

--- api/index.php
+++ api/index.php
@@ -6,6 +6,10 @@ require_once 'config.php';
 $auth_class = AUTH_CLASS;
 $api = new \Doner\API( new $auth_class() );
 
+$api->add_route( 1, \Doner\API::HTTP_GET, 'me', function () use ( $api ) {
+	$api->response->set_body( $api->get_user() );
+} );
+
 $api->add_route( 1, \Doner\API::HTTP_GET, 'dones', function () use ( $api ) {
 	$api->response->set_body( \Doner\Model\Done::get() );
 } );
